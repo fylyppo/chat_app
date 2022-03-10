cfa2b0ed855bc75019d2f0da5e34381af9266978
fylyppo <63547653+fylyppo@users.noreply.github.com>
2022-03-10 16:05:03 +0100

    Add scaffold of create group page

---
+++ lib/screens/create_group_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateGroupPage extends StatelessWidget {
+  const CreateGroupPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('Create group page')),
+    );
+  }
+}
